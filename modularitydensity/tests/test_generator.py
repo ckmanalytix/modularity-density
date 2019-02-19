79956a3c0744b56bae5343c6d210b151707feee0
Swathi Mula <smula@ckmadvisors.com>
2019-02-19 12:00:32 -0500

    added more tests

--- modularitydensity/tests/test_generator.py
+++ modularitydensity/tests/test_generator.py
@@ -4,6 +4,7 @@
 import networkx as nx
 import numpy as np
 from nose.tools import assert_equal
+from nose.tools import raises
 
 import sys
 sys.path.append("..")
@@ -22,143 +23,173 @@ class TestQds(object):
         self.G = nx.karate_club_graph()
         self.adj = nx.to_scipy_sparse_matrix(self.G)
 
-        gr_john = np.array([8, 9, 14, 15, 18, 20, 22,
-                           23, 26, 27, 28, 29, 30, 32, 33])
-
-        gr_hi = np.array([0, 1, 2, 3, 7, 11, 12, 13, 17, 19, 21])
-
-        gr_3 = np.array([24, 25, 31])
-
-        gr_4 = np.array([4, 5, 6, 10, 16])
-
-        self.c = np.zeros(self.adj.shape[0], dtype=int)
-        self.c[gr_john] = 0
-        self.c[gr_hi] = 1
-        self.c[gr_3] = 2
-        self.c[gr_4] = 3
-
-        self.expected = set({frozenset(gr_john), frozenset(gr_hi),
-                            frozenset(gr_3), frozenset(gr_4)})
-
-    def _check_communities(self, communities):
-        assert_equal(self.expected, communities)
-
     def test_fine_tuned_modularity_density(self):
 
-        community_array = fine_tuned_clustering_qds(self.G, normalize=True,
-                                                    evd_method='lanczos',
-                                                    tolerence=1e-08, seed=100)
+        community_array = fine_tuned_clustering_qds(self.G, normalize=False,
+                                                    seed=100)
 
-        communities = []
-        for label in np.unique(community_array):
-            communities.append(frozenset(np.where(community_array
-                                                  == label)[0]))
+        computed_metric = modularity_density(self.adj, community_array,
+                                             np.unique(community_array))
 
-        self._check_communities(set(communities))
+        assert_equal(round(computed_metric, 4), 0.2382)
 
-    def test_modularity_density(self):
+    def test_fine_tuned_modularity_density_normalized(self):
 
-        computed_metric = modularity_density(self.adj, self.c,
-                                             np.unique(self.c))
+        community_array = fine_tuned_clustering_qds(self.G, seed=100)
+
+        computed_metric = modularity_density(self.adj, community_array,
+                                             np.unique(community_array))
 
         assert_equal(round(computed_metric, 4), 0.2313)
 
+    def test_fine_tuned_modularity_density_method2(self):
 
-class TestQ(object):
+        community_array = fine_tuned_clustering_qds(self.G,
+                                                    evd_method='tracemin',
+                                                    seed=100)
 
-    def setup(self):
-        self.G = nx.karate_club_graph()
-        self.adj = nx.to_scipy_sparse_matrix(self.G)
+        computed_metric = modularity_density(self.adj, community_array,
+                                             np.unique(community_array))
 
-        gr_john = np.array([8, 9, 14, 15, 18, 20, 22,
-                            26, 29, 30, 32, 33])
+        assert_equal(round(computed_metric, 4), 0.2313)
 
-        gr_hi = np.array([0, 1, 2, 3, 7, 11, 12, 13, 17, 19, 21])
+    def test_fine_tuned_modularity_density_method3(self):
 
-        gr_3 = np.array([23, 24, 25, 27, 28, 31])
+        community_array = fine_tuned_clustering_qds(self.G,
+                                                    evd_method='lobpcg',
+                                                    seed=100)
 
-        gr_4 = np.array([4, 5, 6, 10, 16])
+        computed_metric = modularity_density(self.adj, community_array,
+                                             np.unique(community_array))
 
-        self.c = np.zeros(self.adj.shape[0], dtype=int)
-        self.c[gr_john] = 0
-        self.c[gr_hi] = 1
-        self.c[gr_3] = 2
-        self.c[gr_4] = 3
+        assert_equal(round(computed_metric, 4), 0.2313)
 
-        self.expected = set({frozenset(gr_john), frozenset(gr_hi),
-                            frozenset(gr_3), frozenset(gr_4)})
+    def test_fine_tuned_modularity_density_tolerence(self):
 
-    def _check_communities(self, communities):
-        assert_equal(self.expected, communities)
+        community_array = fine_tuned_clustering_qds(self.G, tolerence=1e-04,
+                                                    seed=100)
+
+        computed_metric = modularity_density(self.adj, community_array,
+                                             np.unique(community_array))
+
+        assert_equal(round(computed_metric, 4), 0.2313)
+
+    def test_fine_tuned_modularity_density_seed(self):
+
+        community_array = fine_tuned_clustering_qds(self.G, seed=2)
+
+        computed_metric = modularity_density(self.adj, community_array,
+                                             np.unique(community_array))
+
+        assert_equal(round(computed_metric, 4), 0.2313)
+
+    def test_fine_tuned_modularity_density_2(self):
+
+        community_array = fine_tuned_clustering_qds(self.G, normalize=False,
+                                                    evd_method='lobpcg',
+                                                    seed=2)
+
+        computed_metric = modularity_density(self.adj, community_array,
+                                             np.unique(community_array))
+
+        assert_equal(round(computed_metric, 4), 0.2382)
 
     def test_fine_tuned_modularity(self):
 
-        community_array = fine_tuned_clustering_q(self.G, r=0,
-                                                  normalize=True,
-                                                  evd_method='lanczos',
-                                                  tolerence=1e-08, seed=100)
+        community_array = fine_tuned_clustering_q(self.G, normalize=False,
+                                                  seed=100)
 
-        communities = []
-        for label in np.unique(community_array):
-            communities.append(frozenset(np.where(community_array
-                                                  == label)[0]))
-
-        self._check_communities(set(communities))
-
-    def test_modularity(self):
-
-        computed_metric = modularity_r(self.adj, self.c,
-                                       np.unique(self.c), r=0)
+        computed_metric = modularity_r(self.adj, community_array,
+                                       np.unique(community_array))
 
         assert_equal(round(computed_metric, 4), 0.4198)
 
+    def test_fine_tuned_modularity_normalized(self):
 
-class TestQRescaled(object):
+        community_array = fine_tuned_clustering_q(self.G, seed=100)
 
-    def setup(self):
-        self.G = nx.karate_club_graph()
-        self.adj = nx.to_scipy_sparse_matrix(self.G)
+        computed_metric = modularity_r(self.adj, community_array,
+                                       np.unique(community_array))
 
-        gr_john = np.array([8, 14, 15, 18, 20, 22, 26, 29, 30, 32, 33])
+        assert_equal(round(computed_metric, 4), 0.4198)
 
-        gr_hi = np.array([0, 1, 2, 3, 7, 11, 12, 13, 17, 19, 21])
+    def test_fine_tuned_modularity_method2(self):
 
-        gr_3 = np.array([23, 24, 25, 27, 28, 31])
+        community_array = fine_tuned_clustering_q(self.G,
+                                                  evd_method='tracemin',
+                                                  seed=100)
 
-        gr_4 = np.array([4, 5, 6, 10, 16])
+        computed_metric = modularity_r(self.adj, community_array,
+                                       np.unique(community_array))
 
-        gr_5 = np.array([9])
+        assert_equal(round(computed_metric, 4), 0.4198)
 
-        self.c = np.zeros(self.adj.shape[0], dtype=int)
-        self.c[gr_john] = 0
-        self.c[gr_hi] = 1
-        self.c[gr_3] = 2
-        self.c[gr_4] = 3
-        self.c[gr_5] = 4
+    def test_fine_tuned_modularity_method3(self):
 
-        self.expected = set({frozenset(gr_john), frozenset(gr_hi),
-                            frozenset(gr_3), frozenset(gr_4), frozenset(gr_5)})
+        community_array = fine_tuned_clustering_q(self.G,
+                                                  evd_method='lobpcg',
+                                                  seed=100)
 
-    def _check_communities(self, communities):
-        assert_equal(self.expected, communities)
+        computed_metric = modularity_r(self.adj, community_array,
+                                       np.unique(community_array))
+
+        assert_equal(round(computed_metric, 4), 0.4198)
+
+    def test_fine_tuned_modularity_tolerence(self):
+
+        community_array = fine_tuned_clustering_q(self.G, tolerence=1e-04,
+                                                  seed=100)
+
+        computed_metric = modularity_r(self.adj, community_array,
+                                       np.unique(community_array))
+
+        assert_equal(round(computed_metric, 4), 0.4198)
 
     def test_fine_tuned_modularity_r(self):
 
-        community_array = fine_tuned_clustering_q(self.G, r=2.0,
-                                                  normalize=True,
-                                                  evd_method='lanczos',
-                                                  tolerence=1e-08, seed=100)
+        community_array = fine_tuned_clustering_q(self.G, r=2.0, seed=100)
 
-        communities = []
-        for label in np.unique(community_array):
-            communities.append(frozenset(np.where(community_array
-                               == label)[0]))
-
-        self._check_communities(set(communities))
-
-    def test_modularity_r(self):
-
-        computed_metric = modularity_r(self.adj, self.c,
-                                       np.unique(self.c), r=2.0)
+        computed_metric = modularity_r(self.adj, community_array,
+                                       np.unique(community_array), r=2.0)
 
         assert_equal(round(computed_metric, 4), 0.5148)
+
+    def test_fine_tuned_modularity_seed(self):
+
+        community_array = fine_tuned_clustering_q(self.G, seed=2)
+
+        computed_metric = modularity_r(self.adj, community_array,
+                                       np.unique(community_array))
+
+        assert_equal(round(computed_metric, 4), 0.4198)
+
+    def test_fine_tuned_modularity_2(self):
+
+        community_array = fine_tuned_clustering_q(self.G, normalize=False,
+                                                  r=2, evd_method='lobpcg',
+                                                  seed=2)
+
+        computed_metric = modularity_r(self.adj, community_array,
+                                       np.unique(community_array), r=2)
+
+        assert_equal(round(computed_metric, 4), 0.5153)
+
+    @raises(nx.NetworkXNotImplemented)
+    def test_fine_tuned_modularity_density_exception1(self):
+        Gr = nx.DiGraph()
+        fine_tuned_clustering_qds(Gr)
+
+    @raises(nx.NetworkXNotImplemented)
+    def test_fine_tuned_modularity_density_exception2(self):
+        Gr = nx.MultiGraph()
+        fine_tuned_clustering_qds(Gr)
+
+    @raises(nx.NetworkXNotImplemented)
+    def test_fine_tuned_modularity_exception1(self):
+        Gr = nx.DiGraph()
+        fine_tuned_clustering_qds(Gr)
+
+    @raises(nx.NetworkXNotImplemented)
+    def test_fine_tuned_modularity_exception2(self):
+        Gr = nx.MultiGraph()
+        fine_tuned_clustering_qds(Gr)

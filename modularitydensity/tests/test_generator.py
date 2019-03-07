75f2884b7901ff5e256151b9d3a27193adc35d46
Swathi Mula <smula@ckmadvisors.com>
2019-03-07 09:54:45 -0500

    added unittests of contrained versions of fine_tuned metric optimizations

--- modularitydensity/tests/test_generator.py
+++ modularitydensity/tests/test_generator.py
@@ -15,6 +15,10 @@ from fine_tuned_modularity_density \
  import fine_tuned_clustering_qds
 from fine_tuned_modularity \
  import fine_tuned_clustering_q
+from constrained_fine_tuned_modularity \
+ import constrained_fine_tuned_clustering_q
+from constrained_fine_tuned_modularity_density \
+ import constrained_fine_tuned_clustering_qds
 
 
 class Test_all(object):
@@ -174,6 +178,54 @@ class Test_all(object):
 
         assert_equal(round(computed_metric, 4), 0.5153)
 
+    def test_constrained_fine_tuned_modularity_density_size1(self):
+
+        community_array = \
+                 constrained_fine_tuned_clustering_qds(self.G,
+                                                       cluster_size=15,
+                                                       seed=100)
+
+        computed_metric = modularity_density(self.adj, community_array,
+                                             np.unique(community_array))
+
+        assert_equal(round(computed_metric, 4), 0.2313)
+
+    def test_constrained_fine_tuned_modularity_density_size2(self):
+
+        community_array = \
+                constrained_fine_tuned_clustering_qds(self.G,
+                                                      cluster_size=13,
+                                                      seed=100)
+
+        computed_metric = modularity_density(self.adj, community_array,
+                                             np.unique(community_array))
+
+        assert_equal(round(computed_metric, 4), 0.2230)
+
+    def test_constrained_fine_tuned_modularity_size1(self):
+
+        community_array = \
+            constrained_fine_tuned_clustering_q(self.G,
+                                                cluster_size=15,
+                                                seed=100)
+
+        computed_metric = modularity_r(self.adj, community_array,
+                                       np.unique(community_array))
+
+        assert_equal(round(computed_metric, 4), 0.4198)
+
+    def test_constrained_fine_tuned_modularity_size2(self):
+
+        community_array = \
+                    constrained_fine_tuned_clustering_q(self.G,
+                                                        cluster_size=10,
+                                                        seed=100)
+
+        computed_metric = modularity_r(self.adj, community_array,
+                                       np.unique(community_array))
+
+        assert_equal(round(computed_metric, 4), 0.3909)
+
     @raises(nx.NetworkXNotImplemented)
     def test_fine_tuned_modularity_density_exception1(self):
         Gr = nx.DiGraph()
@@ -193,3 +245,23 @@ class Test_all(object):
     def test_fine_tuned_modularity_exception2(self):
         Gr = nx.MultiGraph()
         fine_tuned_clustering_qds(Gr)
+
+    @raises(nx.NetworkXNotImplemented)
+    def test_constrained_fine_tuned_modularity_density_exception1(self):
+        Gr = nx.DiGraph()
+        constrained_fine_tuned_clustering_qds(Gr, cluster_size=100)
+
+    @raises(nx.NetworkXNotImplemented)
+    def test_constrained_fine_tuned_modularity_density_exception2(self):
+        Gr = nx.MultiGraph()
+        constrained_fine_tuned_clustering_qds(Gr, cluster_size=100)
+
+    @raises(nx.NetworkXNotImplemented)
+    def test_constrained_fine_tuned_modularity_exception1(self):
+        Gr = nx.DiGraph()
+        constrained_fine_tuned_clustering_q(Gr, cluster_size=100)
+
+    @raises(nx.NetworkXNotImplemented)
+    def test_constrained_fine_tuned_modularity_exception2(self):
+        Gr = nx.MultiGraph()
+        constrained_fine_tuned_clustering_q(Gr, cluster_size=100)
